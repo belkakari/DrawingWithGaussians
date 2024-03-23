d201814a7016d561e6b2b52bd332569fa13476cb
Gleb <gleb@deepcake.io>
2024-03-23 20:02:15 +0200

    fix

--- fit.py
+++ fit.py
@@ -12,21 +12,22 @@ from drawingwithgaussians.rendering2d import pixel_loss, rasterize, split_gaussi
 seed = 1
 key = jax.random.key(seed)
 
-path = "/Users/gleb/Downloads/eye.jpeg"
+path = "/Users/gleb/Downloads/avataro4ka.jpg"
 img = Image.open(path)
 
 height = 128
 width = 128
 angle = 0.0
-num_gaussians = 1
-lr = 1e-2
+num_gaussians = 2
+lr = 1e-1
+num_epochs = 10
 max_steps = 2000
-learning_rate_schedule = optax.constant_schedule(
-    lr
+learning_rate_schedule = optax.cosine_decay_schedule(
+    lr / 1000, max_steps
 )  # cosine_onecycle_schedule(max_steps, lr)
 
 target_image = jnp.array(img.resize((height, width)), dtype=jnp.float32)[:, :, :3] / 255
-background_color = target_image.mean(0).mean(0)[None, None, :]
+background_color = target_image.mean(0).mean(0)[None, None, :] * 0.0
 means = jax.random.uniform(
     key, (num_gaussians, 2), minval=0, maxval=height, dtype=jnp.float32
 )
@@ -59,13 +60,14 @@ opt_state_background = optimize_background.init(background_color)
 
 prev_stats = []
 frames = []
-for num_epoch in range(10):
+out = cv2.VideoWriter(
+    "outpy.avi", cv2.VideoWriter_fourcc("M", "J", "P", "G"), 24, (width, height)
+)
+for num_epoch in range(num_epochs):
     for step in range(max_steps):
         (loss, renderred_gaussians), gradients = jax.value_and_grad(
             pixel_loss, argnums=[0, 1, 2, 3, 4], has_aux=True
         )(means, L, colors, rotmats, background_color, target_image)
-
-        frames.append(renderred_gaussians)
 
         updates_means, opt_state_means = optimize_means.update(
             gradients[0], opt_state_means
@@ -99,6 +101,9 @@ for num_epoch in range(10):
             break
         if step % 50 == 0:
             print(loss, step, num_epoch, means.shape[0])
+            out.write(
+                np.array((renderred_gaussians[:, :, ::-1] * 255).astype(np.uint8))
+            )
         prev_stats = [
             (jnp.linalg.norm(gradient), gradient.max()) for gradient in gradients
         ]
@@ -107,33 +112,36 @@ for num_epoch in range(10):
     scovs = []
     scolors = []
     srotmats = []
-    covariances = L.at[:, 0, 1].set(0) @ jnp.transpose(
-        L.at[:, 0, 1].set(0), axes=[0, 2, 1]
-    )
-    for mean, cov, color, rotmat in zip(means, covariances, colors, rotmats):
-        mean, cov, color, rotmat = split_gaussian(mean, cov, color, rotmat)
-        smeans.append(mean.reshape(2, -1))
-        scovs.append(cov.reshape(2, 2, 2))
-        scolors.append(color.reshape(2, -1))
-        srotmats.append(rotmat.reshape(2, 2, 2))
+    if num_epoch < num_epochs // 2 + 1:
+        covariances = L.at[:, 0, 1].set(0) @ jnp.transpose(
+            L.at[:, 0, 1].set(0), axes=[0, 2, 1]
+        )
+        for mean, cov, color, rotmat in zip(means, covariances, colors, rotmats):
+            mean, cov, color, rotmat = split_gaussian(mean, cov, color, rotmat)
+            smeans.append(mean.reshape(2, -1))
+            scovs.append(cov.reshape(2, 2, 2))
+            scolors.append(color.reshape(2, -1))
+            srotmats.append(rotmat.reshape(2, 2, 2))
 
-    means, covariances, colors, rotmats = (
-        jnp.concatenate(smeans),
-        jnp.concatenate(scovs),
-        jnp.concatenate(scolors),
-        jnp.concatenate(srotmats),
-    )
-    L = jax.lax.linalg.cholesky(covariances)
+        means, covariances, colors, rotmats = (
+            jnp.concatenate(smeans),
+            jnp.concatenate(scovs),
+            jnp.concatenate(scolors),
+            jnp.concatenate(srotmats),
+        )
+        L = jax.lax.linalg.cholesky(covariances)
+
+    learning_rate_schedule = optax.cosine_onecycle_schedule(max_steps, lr)
+
+    optimize_means = optax.adam(learning_rate_schedule)
+    optimize_cov = optax.adam(learning_rate_schedule)
+    optimize_colors = optax.adam(learning_rate_schedule)
+    optimize_rotmats = optax.adam(learning_rate_schedule)
+    optimize_background = optax.adam(learning_rate_schedule)
 
     opt_state_means = optimize_means.init(means)
     opt_state_cov = optimize_cov.init(L)
     opt_state_colors = optimize_colors.init(colors)
     opt_state_rotmats = optimize_rotmats.init(rotmats)
     opt_state_background = optimize_background.init(background_color)
-
-out = cv2.VideoWriter(
-    "outpy.avi", cv2.VideoWriter_fourcc("M", "J", "P", "G"), 24, (width, height)
-)
-for frame in frames:
-    out.write(np.array((frame[:, :, ::-1] * 255).astype(np.uint8)))
 out.release()
